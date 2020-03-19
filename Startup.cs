5562c4744d1b14523c6150973ca176e637383105
dtirnanic <59708236+dtirnanic@users.noreply.github.com>
2020-03-18 20:54:22 -0500

    Implemented GlobalRouting class to configure routing to users respective index upon login

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,8 @@ using RPM_Tool.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
 
 namespace RPM_Tool
 {
@@ -35,6 +37,10 @@ namespace RPM_Tool
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
 
+            services.AddScoped<ClaimsPrincipal>(s => s.GetService<IHttpContextAccessor>().HttpContext.User);
+
+            services.AddControllers(config => config.Filters.Add(typeof(GlobalRouting)));
+       	
 
             services.AddControllersWithViews();
             services.AddRazorPages();
